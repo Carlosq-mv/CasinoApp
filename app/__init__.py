5673810fd0c055f2d750dad67b3163c051409f17
Brandon Rivera <brandon.riveravenegas@sjsu.edu>
2024-03-06 17:41:06 -0800

    added password confirmation

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 # from flask_admin import Admin # can use this later
-from flask_admin.contrib.sqla import ModelView
 from datetime import datetime
 from dotenv import load_dotenv
 import os
